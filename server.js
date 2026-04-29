3cf255271e88c0db15adc67e7a6d73d08476cd52
shailu-18 <rooparoopa786000@gmail.com>
2026-04-30 04:15:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
