108c9efb4fb96c712e4f33dcd219b318b900c55d
shailu-18 <rooparoopa786000@gmail.com>
2026-05-23 08:00:00 +0530

    fixed changes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,95 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-const router = express.Router();
-
-const bookingSchema = new mongoose.Schema({
-  bookingId: String,
-  name: String,
-  email: String,
-  contact: String,
-  date: String,
-  destination: String,
-  duration: String,
-  adults: Number,
-  children: Number,
-  totalPrice: Number
-});
-
-const Booking =
-mongoose.models.Booking ||
-mongoose.model(
-  "Booking",
-  bookingSchema
-);
-
-router.delete("/:id", async(req,res)=>{
-
-try{
-
-await Booking.findByIdAndDelete(
-req.params.id
-);
-
-res.json({
-message:"Deleted successfully"
-});
-
-}
-catch(err){
-
-res.status(500).json({
-message:err.message
-});
-
-}
-
-});
-
-// CREATE
-router.post("/", async (req,res)=>{
-
-try{
-
-const booking=new Booking(req.body);
-
-await booking.save();
-
-res.status(201).json({
-message:"Booking saved",
-data:booking
-});
-
-}
-catch(err){
-
-res.status(500).json({
-message:"Booking save failed",
-error:err.message
-});
-
-}
-
-});
-
-// GET ALL
 router.get("/",async(req,res)=>{
 
 try{
 
-const data=await Booking.find();
+const email=
+req.query.email;
 
-res.json(data);
+const data=
+await Booking.find({
+email:email
+});
+
+res.json(
+data
+);
 
 }
 catch(err){
 
-res.status(500).json({
-message:err.message
+res.status(500)
+.json({
+message:
+err.message
 });
 
 }
 
 });
-
-module.exports=router;
